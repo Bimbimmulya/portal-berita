3d510720383f907c01461978ef363442a83dde7a
bimbimmulya <bimbimmulya24@gmail.com>
2021-07-02 13:57:16 +0700

    copyright

--- resources/views/template/app.blade.php
+++ resources/views/template/app.blade.php
@@ -102,11 +102,11 @@
                         <div class="footer-copyright">
                             <span>&copy;
                                 <!-- Link back to Colorlib cant be removed. Template is licensed under CC BY 3.0. -->
-                                Copyright &copy;<script>
+                                 <script>
                                     document.write(new Date().getFullYear());
 
-                                </script> All rights reserved | This template is made with <i class="fa fa-heart-o"
-                                    aria-hidden="true"></i> by <a href="https://colorlib.com"
+                                </script> Aktualisasi | Made with <i class="fa fa-heart-o"
+                                    aria-hidden="true"></i> using <a href="https://colorlib.com"
                                     target="_blank">Colorlib</a>
                         </div>
                     </div>
